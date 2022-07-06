7e6e28f7f61c232ac49a52b9be16cb05067008ec
Kopselek <konrad.kkacperek@interia.pl>
2022-07-06 22:18:37 +0200

    Initial commit - test for OOP abstraction form

---
+++ Site/Util/Input/BaseInput.php
@@ -0,0 +1,14 @@
+<?php
+abstract class BaseInput extends HtmlElement
+{
+    public string $name;
+    public string $label;
+    public string $value;
+
+    public function __construct(string $name, string $label = '', string $value = '')
+    {
+        $this->name = $name;
+        $this->label = $label;
+        $this->value = $value;
+    }
+}
--- index.php
+++ index.php
@@ -1,4 +1,36 @@
-<?php session_start();
-unset($_SESSION["login"]);
-session_destroy();
-echo file_get_contents("public/index.html");
+<?php
+require_once __DIR__ . "/Site/Util/HtmlElement.php";
+require_once __DIR__ . "/Site/Util/Form.php";
+require_once __DIR__ . "/Site/Util/Mist/Button.php";
+require_once __DIR__ . "/Site/Util/Input/BaseInput.php";
+require_once __DIR__ . "/Site/Util/Input/TextInput.php";
+require_once __DIR__ . "/Site/Util/Input/PasswordInput.php";
+
+$form = new Form();
+$form->addElement(new TextInput('login', 'Login'));
+$form->addElement(new PasswordInput('password', 'Password'));
+$form->addElement(new Button('Submit'));
+
+//session_start();
+// if (isset($_SESSION["login"])) {
+//     //logged
+// } else {
+//     //unlogged
+// }
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Ticket-Panel</title>
+</head>
+
+<body>
+    <?php echo $form->render() ?>
+</body>
+
+</html>
